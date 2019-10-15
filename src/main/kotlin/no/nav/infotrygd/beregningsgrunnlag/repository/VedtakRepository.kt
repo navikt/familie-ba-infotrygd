61adb3b66b717475480030988c407a2394c68861
Alexander Raknes <aleksander.raknes@nav.no>
2019-10-15 08:36:28 +0200

    Oppdatert SQL for å liste vedtak

--- src/main/kotlin/no/nav/infotrygd/beregningsgrunnlag/repository/VedtakRepository.kt
+++ src/main/kotlin/no/nav/infotrygd/beregningsgrunnlag/repository/VedtakRepository.kt
@@ -20,7 +20,7 @@ interface VedtakRepository : JpaRepository<Vedtak, Long> {
         SELECT v FROM Vedtak v
          WHERE v.person.fnr = :fnr
            AND v.datoStart >= :fom
-           AND v.stonad.kodeRutine in ('BS', 'BR')
+           AND v.stonad.kodeRutine = 'BS'
     """)
     fun findByFnrAndStartDato(fnr: FoedselNr, fom: LocalDate): List<Vedtak>
 
@@ -29,7 +29,7 @@ interface VedtakRepository : JpaRepository<Vedtak, Long> {
          WHERE v.person.fnr = :fnr
            AND v.datoStart >= :fom
            AND v.datoStart <= :tom
-           AND v.stonad.kodeRutine in ('BS', 'BR')
+           AND v.stonad.kodeRutine = 'BS'
     """)
     fun findByFnrAndStartDato(fnr: FoedselNr, fom: LocalDate, tom: LocalDate): List<Vedtak>
 }
