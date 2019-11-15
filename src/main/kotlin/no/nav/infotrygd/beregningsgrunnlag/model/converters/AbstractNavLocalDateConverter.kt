4b8e6124842790ab5777cc5357fef84b6083e00e
Alexander Raknes <aleksander.raknes@nav.no>
2019-11-15 12:54:38 +0100

    Lagt til try/catch for datokonvertering

--- src/main/kotlin/no/nav/infotrygd/beregningsgrunnlag/model/converters/AbstractNavLocalDateConverter.kt
+++ src/main/kotlin/no/nav/infotrygd/beregningsgrunnlag/model/converters/AbstractNavLocalDateConverter.kt
@@ -1,10 +1,13 @@
 package no.nav.infotrygd.beregningsgrunnlag.model.converters
 
+import org.slf4j.LoggerFactory
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import javax.persistence.AttributeConverter
 
 open class AbstractNavLocalDateConverter(datePattern: String) : AttributeConverter<LocalDate?, Int?> {
+    private val logger = LoggerFactory.getLogger(javaClass)
+    
     private val formatter = DateTimeFormatter.ofPattern(datePattern);
     override fun convertToDatabaseColumn(attribute: LocalDate?): Int? {
         return attribute?.format(formatter)?.toInt()
@@ -14,7 +17,17 @@ open class AbstractNavLocalDateConverter(datePattern: String) : AttributeConvert
         if(dbData == null || dbData == NULL_VALUE) {
             return null
         }
-        return LocalDate.from(formatter.parse(String.format("%08d", dbData)))
+        return try {
+            LocalDate.from(formatter.parse(String.format("%08d", dbData)))
+        } catch (e: Exception) {
+            /*
+                Det finnes datoer i databasen som er ugyldige fordi at de er ført feil i kombinasjon
+                med manglende input-validering. I disse tilfellene så har vi ikke informasjon om dato
+                så vi returnerer et tomt resultat.
+             */
+            logger.warn("Kunne ikke lese dato: '$dbData'")
+            null;
+        }
     }
 
     companion object {
