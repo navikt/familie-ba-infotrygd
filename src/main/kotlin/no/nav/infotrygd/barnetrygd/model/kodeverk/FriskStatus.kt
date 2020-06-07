eb42a82c8a5f9b5aaf43b5295855e9d16fa3c7be
Tor Nærland <tor.nerland@nav.no>
2020-06-08 00:19:02 +0200

    Fjernet alt som ikke har med barnetrygd å gjøre

--- src/main/kotlin/no/nav/infotrygd/barnetrygd/model/kodeverk/FriskStatus.kt
+++
@@ -1,7 +0,0 @@
-package no.nav.infotrygd.barnetrygd.model.kodeverk
-
-enum class FriskStatus(override val kode: String, override val tekst: String) : Kode {
-    LOEPENDE("L", "Løpende"),
-    AVSLUTTET("A", "Avsluttet"),
-    IKKE_STARTET("I", "Ikke startet")
-}
