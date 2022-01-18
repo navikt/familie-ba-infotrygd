cf9004f8003ce1762ae8d7e48a89878109fe412a
Tor Nærland <tor.nerland@nav.no>
2022-01-18 17:12:19 +0100

    Migrerer fra springfox til springdoc og legger til støtte for oauth2 autentisering

--- src/main/kotlin/no/nav/familie/ba/infotrygd/config/SecurityConfiguration.kt
+++ src/main/kotlin/no/nav/familie/ba/infotrygd/config/SecurityConfiguration.kt
@@ -1,11 +1,65 @@
 package no.nav.familie.ba.infotrygd.config
 
 import no.nav.familie.ba.infotrygd.Profiles
+import no.nav.security.token.support.spring.SpringTokenValidationContextHolder
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
-@EnableJwtTokenValidation(ignore = ["org.springframework", "springfox"])
+@EnableJwtTokenValidation(ignore = ["org.springframework", "springfox", "org.springdoc"])
 @Profile("!${Profiles.NOAUTH}")
 @Configuration
-class SecurityConfiguration
+class SecurityConfiguration(
+    @Value("\${rolle.teamfamilie.forvalter}")
+    val forvalterRolleTeamfamilie: String
+) {
+
+    private val secureLogger = LoggerFactory.getLogger("secureLogger")
+
+    @Bean
+    fun requestFilter() = object : OncePerRequestFilter() {
+        override fun doFilterInternal(
+            request: HttpServletRequest,
+            response: HttpServletResponse,
+            filterChain: FilterChain
+        ) {
+            val grupper = hentGrupper()
+            if (!grupper.contains(forvalterRolleTeamfamilie) && !environment.activeProfiles.contains("test")) {
+                secureLogger.info("Ugyldig rolle for url=${request.requestURI} grupper=$grupper, forvalterRolleTeamfamilie=$forvalterRolleTeamfamilie")
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Handling krever teamfamilie forvalter rolle")
+            } else {
+                try {
+                    filterChain.doFilter(request, response)
+                } catch (e: Exception) {
+                    secureLogger.warn("Uventet feil i doFilter for url=${request.requestURI} grupper=$grupper", e)
+                    throw e
+                }
+            }
+        }
+
+        override fun shouldNotFilter(request: HttpServletRequest): Boolean {
+            return request.requestURI.contains("/internal") ||
+                    request.requestURI.contains("/actuator") ||
+                    request.requestURI.startsWith("/swagger") ||
+                    request.requestURI.startsWith("/v3")
+        } // i bruk av swagger
+    }
+
+    private fun hentGrupper(): List<String> {
+        return Result.runCatching { SpringTokenValidationContextHolder().tokenValidationContext }
+            .fold(
+                onSuccess = {
+                    @Suppress("UNCHECKED_CAST")
+                    it?.getClaims("azure")?.get("groups") as List<String>? ?: emptyList()
+                },
+                onFailure = { emptyList() }
+            )
+    }
+}
--- src/main/kotlin/no/nav/familie/ba/infotrygd/rest/controller/BarnetrygdController.kt
+++ src/main/kotlin/no/nav/familie/ba/infotrygd/rest/controller/BarnetrygdController.kt
@@ -1,9 +1,9 @@
 package no.nav.familie.ba.infotrygd.rest.controller
 
 import io.micrometer.core.annotation.Timed
-import io.swagger.annotations.ApiImplicitParam
-import io.swagger.annotations.ApiImplicitParams
-import io.swagger.annotations.ApiOperation
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.ExampleObject
 import no.nav.commons.foedselsnummer.FoedselsNr
 import no.nav.familie.ba.infotrygd.rest.api.InfotrygdLøpendeBarnetrygdResponse
 import no.nav.familie.ba.infotrygd.rest.api.InfotrygdÅpenSakResponse
@@ -35,12 +35,13 @@ class BarnetrygdController(
 ) {
     private val logger = LoggerFactory.getLogger(javaClass)
 
-    @ApiOperation("Avgjør hvorvidt det finnes løpende barnetrygd på søker eller barn i Infotrygd.")
+    @Operation(summary = "Avgjør hvorvidt det finnes løpende barnetrygd på søker eller barn i Infotrygd.")
     @PostMapping(path = ["lopende-barnetrygd"], consumes = ["application/json"])
-    @ApiImplicitParams(
-        ApiImplicitParam(name = "request",
-            dataType = "InfotrygdSøkRequest",
-            value = "{\n  \"brukere\": [\"12345678910\"]," + "\n  \"barn\": [\n\"23456789101\",\n\"34567891012\"\n]\n}"))
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+        content = [Content(
+            examples = [ExampleObject(value = "{\n  \"brukere\": [\"12345678910\"]," + "\n  \"barn\": [\n\"23456789101\",\n\"34567891012\"\n]\n}")]
+        )]
+    )
     fun harLopendeBarnetrygd(@RequestBody request: InfotrygdSøkRequest): ResponseEntity<InfotrygdLøpendeBarnetrygdResponse> {
         clientValidator.authorizeClient()
 
@@ -50,12 +51,13 @@ class BarnetrygdController(
 
         return ResponseEntity.ok(InfotrygdLøpendeBarnetrygdResponse(harLøpendeBarnetrygd))
     }
-    @ApiOperation("Svarer hvorvidt det finnes en åpen sak til beslutning, på søker eller barn i Infotrygd.")
+    @Operation(summary = "Svarer hvorvidt det finnes en åpen sak til beslutning, på søker eller barn i Infotrygd.")
     @PostMapping(path = ["aapen-sak"], consumes = ["application/json"])
-    @ApiImplicitParams(
-        ApiImplicitParam(name = "request",
-            dataType = "InfotrygdSøkRequest",
-            value = "{\n  \"brukere\": [\"12345678910\"]," + "\n  \"barn\": [\n\"23456789101\",\n\"34567891012\"\n]\n}"))
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+        content = [Content(
+            examples = [ExampleObject(value = "{\n  \"brukere\": [\"12345678910\"]," + "\n  \"barn\": [\n\"23456789101\",\n\"34567891012\"\n]\n}")]
+        )]
+    )
     fun harÅpenSak(@RequestBody request: InfotrygdSøkRequest): ResponseEntity<InfotrygdÅpenSakResponse> {
         clientValidator.authorizeClient()
 
@@ -64,14 +66,17 @@ class BarnetrygdController(
         }
     }
 
-    @ApiOperation("Uttrekk fra tabellen \"BA_STOENAD_20\".")
-        @PostMapping(path = ["stonad"], consumes = ["application/json"])
-    @ApiImplicitParams(
-        ApiImplicitParam(name = "request",
-            dataType = "InfotrygdSøkRequest",
-            value = "{\n  \"brukere\": [\"12345678910\"]," + "\n  \"barn\": [\n\"23456789101\",\n\"34567891012\"\n]\n}"))
-    fun stønad(@RequestBody request: InfotrygdSøkRequest,
-               @RequestParam(required = false) historikk: Boolean?): ResponseEntity<InfotrygdSøkResponse<StønadDto>> {
+    @Operation(summary = "Uttrekk fra tabellen \"BA_STOENAD_20\".")
+    @PostMapping(path = ["stonad"], consumes = ["application/json"])
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+        content = [Content(
+            examples = [ExampleObject(value = "{\n  \"brukere\": [\"12345678910\"]," + "\n  \"barn\": [\n\"23456789101\",\n\"34567891012\"\n]\n}")]
+        )]
+    )
+    fun stønad(
+        @RequestBody request: InfotrygdSøkRequest,
+        @RequestParam(required = false) historikk: Boolean?
+    ): ResponseEntity<InfotrygdSøkResponse<StønadDto>> {
         clientValidator.authorizeClient()
 
         return hentStønaderPåBrukereOgBarn(request.brukere, request.barn, historikk).let {
@@ -79,12 +84,13 @@ class BarnetrygdController(
         }
     }
 
-    @ApiOperation("Uttrekk fra tabellen \"SA_SAK_10\".")
+    @Operation(summary = "Uttrekk fra tabellen \"SA_SAK_10\".")
     @PostMapping(path = ["saker"], consumes = ["application/json"])
-    @ApiImplicitParams(
-        ApiImplicitParam(name = "request",
-            dataType = "InfotrygdSøkRequest",
-            value = "{\n  \"brukere\": [\"12345678910\"]," + "\n  \"barn\": [\n\"23456789101\",\n\"34567891012\"\n]\n}"))
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+        content = [Content(
+            examples = [ExampleObject(value = "{\n  \"brukere\": [\"12345678910\"]," + "\n  \"barn\": [\n\"23456789101\",\n\"34567891012\"\n]\n}")]
+        )]
+    )
     fun saker(@RequestBody request: InfotrygdSøkRequest): ResponseEntity<InfotrygdSøkResponse<SakDto>> {
         clientValidator.authorizeClient()
 
@@ -95,7 +101,7 @@ class BarnetrygdController(
                                            barn = barnetrygdService.findSakerByBarnFnr(barn ?: emptyList())))
     }
 
-    @ApiOperation("Uttrekk personer med ytelse. F.eks OS OS for barnetrygd, UT EF for småbarnstillegg")
+    @Operation(summary = "Uttrekk personer med ytelse. F.eks OS OS for barnetrygd, UT EF for småbarnstillegg")
     @PostMapping(path = ["migrering"])
     fun migrering(@RequestBody request: MigreringRequest): ResponseEntity<Set<String>> {
         clientValidator.authorizeClient()
@@ -113,7 +119,7 @@ class BarnetrygdController(
     }
 
 
-    @ApiOperation("Finn stønad med id")
+    @Operation(summary = "Finn stønad med id")
     @GetMapping(path = ["stonad/{id}"])
     @Deprecated(message="Erstattes av findStønad som henter basert på B01_PERSONKEY, B20_IVERFOM_SEQ, B20_VIRKFOM_SEQ og REGION")
     fun findStønadById(@PathVariable id: Long): ResponseEntity<StønadDto> {
@@ -130,7 +136,7 @@ class BarnetrygdController(
 
     }
 
-    @ApiOperation("Finn stønad basert på personKey, iverksattFom, virkningFom og region")
+    @Operation(summary = "Finn stønad basert på personKey, iverksattFom, virkningFom og region")
     @PostMapping(path = ["stonad/sok"])
     fun findStønad(@RequestBody stønadRequest: StønadRequest): ResponseEntity<StønadDto> {
         clientValidator.authorizeClient()
