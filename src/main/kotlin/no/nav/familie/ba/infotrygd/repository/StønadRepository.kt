67b1b5f3bee722cc023ef1640793dc44777237fc
Stig Helge Strøm <stig.helge.strom@nav.no>
2022-04-05 08:39:28 +0200

    Finner kun stønader som har minst 1 barn

--- src/main/kotlin/no/nav/familie/ba/infotrygd/repository/StønadRepository.kt
+++ src/main/kotlin/no/nav/familie/ba/infotrygd/repository/StønadRepository.kt
@@ -24,7 +24,8 @@ interface StønadRepository : JpaRepository<Stønad, Long> {
     @Query("SELECT new no.nav.familie.ba.infotrygd.repository.TrunkertStønad(s.id, s.personKey, s.fnr, s.sakNr, s.saksblokk, s.status, s.region, s.virkningFom, s.opphørtFom, s.iverksattFom) FROM Stønad s " +
            "WHERE (s.opphørtFom='000000' or CAST(substring(s.opphørtFom, 3, 4) as integer) >= :år) " +
            "AND CAST(substring(s.virkningFom, 1, 4) as integer) >= (9999 - :år) " + //datoformatet er av typen "seq" derav 9999 - år
-           "AND s.status in :statusKoder")
+           "AND s.status in :statusKoder " +
+           "AND s.antallBarn > 0")
     fun findStønadByÅrAndStatusKoder(år: Int, vararg statusKoder: String): List<TrunkertStønad>
 
     @Query("""SELECT s FROM Stønad s
@@ -174,5 +175,5 @@ data class TrunkertStønad(
 
     val opphørtFom: String,
 
-    val iverksattFom: String,
+    val iverksattFom: String
 )
