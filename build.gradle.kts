052167e359ad063714e1513514dd5bc018ab2aac
Stig Helge Strøm <stig.helge.strom@nav.no>
2022-04-08 08:43:45 +0200

    Tst for skatt

--- build.gradle.kts
+++ build.gradle.kts
@@ -95,6 +95,8 @@ dependencies {
     testImplementation("org.testcontainers:oracle-xe:1.16.3")
     testImplementation("io.mockk:mockk:$mockkVersion")
     testImplementation("com.h2database:h2")
+    testImplementation("com.opencsv:opencsv:5.6")
+
 }
 
 tasks.withType<KotlinCompile> {
