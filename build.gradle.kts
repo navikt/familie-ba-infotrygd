349cd9669dff1f4cf55fcccac0275b248f9d20a9
Stig Strøm <stig.helge.strom@nav.no>
2022-11-03 09:53:52 +0100

    mockk-jvm

--- build.gradle.kts
+++ build.gradle.kts
@@ -93,7 +93,7 @@ dependencies {
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.testcontainers:oracle-xe:1.17.3")
-    testImplementation("io.mockk:mockk:$mockkVersion")
+    testImplementation("io.mockk:mockk-jvm:$mockkVersion")
     testImplementation("com.h2database:h2")
     testImplementation("com.opencsv:opencsv:5.6")
 
