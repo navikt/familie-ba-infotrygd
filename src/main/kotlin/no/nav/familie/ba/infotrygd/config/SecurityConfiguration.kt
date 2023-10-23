4027d236070d03656521c8282ad2647b6f44fba5
tnarland <47184872+tnarland@users.noreply.github.com>
2023-10-23 16:51:11 +0200

    Revert "Oppdaterer sikkerhetskonfigurasjonen (#394)"
    This reverts commit b09fdaabc07260fa88d441b7e377596e312352b8.

--- src/main/kotlin/no/nav/familie/ba/infotrygd/config/SecurityConfiguration.kt
+++ src/main/kotlin/no/nav/familie/ba/infotrygd/config/SecurityConfiguration.kt
@@ -1,8 +1,11 @@
 package no.nav.familie.ba.infotrygd.config
 
+import no.nav.familie.ba.infotrygd.Profiles
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 
 @EnableJwtTokenValidation(ignore = ["org.springframework", "springfox", "org.springdoc"])
+@Profile("!${Profiles.NOAUTH}")
 @Configuration
 class SecurityConfiguration
