0bf18a0027b65e843331fc7e9aa53ed8d060da1e
Stig Helge Strøm <stig.helge.strom@nav.no>
2024-04-23 17:07:22 +0200

    Konfigurerer cache for 2021-2023 for skatteendepunktene

--- src/main/kotlin/no/nav/familie/ba/infotrygd/service/WarmupCacheService.kt
+++ src/main/kotlin/no/nav/familie/ba/infotrygd/service/WarmupCacheService.kt
@@ -3,7 +3,6 @@ package no.nav.familie.ba.infotrygd.service
 import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
-import java.time.Year
 
 @Component
 class WarmupCacheService(val barnetrygdService: BarnetrygdService) {
@@ -12,15 +11,15 @@ class WarmupCacheService(val barnetrygdService: BarnetrygdService) {
 
     @Scheduled(fixedDelay = 24 * 60 * 60 * 1000, initialDelay = 10 * 60 * 1000)
     fun warmupSkatteetatenPersonerCache() {
-        val inneværendeÅr = Year.now()
-
-        logger.info("Henter personer med utvidet barnetrygd for å oppdatere cache ${inneværendeÅr.plusYears(1)}")
-        val nextYearResponse = barnetrygdService.finnPersonerUtvidetBarnetrygdSkatt(inneværendeÅr.plusYears(1).toString())
-        logger.info("Henter personer med utvidet barnetrygd for å oppdatere cache $inneværendeÅr")
-        val inneværendeÅrResponse = barnetrygdService.finnPersonerUtvidetBarnetrygdSkatt(inneværendeÅr.toString())
-        logger.info("Henter perioder med utvidet barnetrygd for å oppdatere cache $inneværendeÅr")
-        inneværendeÅrResponse.forEach { barnetrygdService.finnPerioderUtvidetBarnetrygdSkatt(it.ident, inneværendeÅr.value) }
-
-        barnetrygdService.finnPersonerUtvidetBarnetrygdSkatt(inneværendeÅr.minusYears(1).toString())
+        // Varmer opp cache for personer og perioder siste året med særfradrag
+        // Varmer også opp cache for personer for 2021 og 2022. Siden skatt også kaller oss for 2021 og 2022
+        for (år in 2021..2023) {
+            logger.info("Henter personer med utvidet barnetrygd for å oppdatere cache $år")
+            barnetrygdService.finnPersonerUtvidetBarnetrygdSkatt(år.toString()).let { resultat ->
+                if (år == 2023) {
+                    resultat.forEach { barnetrygdService.finnPerioderUtvidetBarnetrygdSkatt(it.ident, 2023) }
+                }
+            }
+        }
     }
 }
