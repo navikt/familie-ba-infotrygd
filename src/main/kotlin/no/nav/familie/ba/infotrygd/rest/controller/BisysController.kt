f46de7bf5116025dd9feabe612dc1a00548ecb02
Stig Helge Strøm <stig.helge.strom@nav.no>
2025-02-12 15:13:28 +0100

    Fjerner 5 års begrensning på infotrygd

--- src/main/kotlin/no/nav/familie/ba/infotrygd/rest/controller/BisysController.kt
+++ src/main/kotlin/no/nav/familie/ba/infotrygd/rest/controller/BisysController.kt
@@ -11,16 +11,12 @@ import no.nav.familie.ba.infotrygd.service.BarnetrygdService
 import no.nav.familie.ba.infotrygd.service.TilgangskontrollService
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.slf4j.LoggerFactory
-import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.server.ResponseStatusException
-import java.time.LocalDateTime
 import java.time.YearMonth
 import io.swagger.v3.oas.annotations.parameters.RequestBody as ApiRequestBody
-
 
 @ProtectedWithClaims(issuer = "azuread")
 @RestController
@@ -28,49 +24,48 @@ import io.swagger.v3.oas.annotations.parameters.RequestBody as ApiRequestBody
 @RequestMapping("/infotrygd/barnetrygd")
 class BisysController(
     private val barnetrygdService: BarnetrygdService,
-    private val tilgangskontrollService: TilgangskontrollService
+    private val tilgangskontrollService: TilgangskontrollService,
 ) {
     private val logger = LoggerFactory.getLogger(javaClass)
 
-    @Operation(summary = "Uttrekk utvidet barnetrygd/småbarnstillegg utbetaling på en person fra en bestemet måned. Maks 5 år tilbake i tid")
+    @Operation(
+        summary = "Uttrekk utvidet barnetrygd/småbarnstillegg utbetaling på en person fra en bestemet måned. Maks 5 år tilbake i tid",
+    )
     @PostMapping(path = ["utvidet"], consumes = ["application/json"])
     @ApiRequestBody(content = [Content(examples = [ExampleObject(value = """{"personIdent": "12345678910", "fraDato": "2020-05"}""")])])
-    @KonsumeresAv(apper = ["familie-ba-sak"] )
-    fun utvidet(@RequestBody request: InfotrygdUtvidetBarnetrygdRequest): InfotrygdUtvidetBarnetrygdResponse {
+    @KonsumeresAv(apper = ["familie-ba-sak"])
+    fun utvidet(
+        @RequestBody request: InfotrygdUtvidetBarnetrygdRequest,
+    ): InfotrygdUtvidetBarnetrygdResponse {
         tilgangskontrollService.sjekkTilgang()
-
-        if (request.fraDato.isBefore(YearMonth.now().minusYears(5)))
-            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "fraDato kan ikke være lenger enn 5 år tilbake i tid")
 
         val bruker = FoedselsNr(request.personIdent)
 
         return barnetrygdService.finnUtvidetBarnetrygdBisys(bruker, request.fraDato)
     }
 
+    data class InfotrygdUtvidetBarnetrygdRequest(
+        val personIdent: String,
+        @Schema(implementation = String::class, example = "2020-05") val fraDato: YearMonth,
+    )
 
-    data class InfotrygdUtvidetBarnetrygdRequest( val personIdent: String,
-                                                  @Schema(implementation = String::class, example = "2020-05") val fraDato: YearMonth)
+    class InfotrygdUtvidetBarnetrygdResponse(
+        val perioder: List<UtvidetBarnetrygdPeriode>,
+    )
 
-
-
-
-    class InfotrygdUtvidetBarnetrygdResponse(val perioder: List<UtvidetBarnetrygdPeriode>)
-
-
-
-    data class UtvidetBarnetrygdPeriode(val stønadstype: Stønadstype,
-                                        @Schema(implementation = String::class, example = "2020-05")
-                                        val fomMåned: YearMonth,
-                                        @Schema(implementation = String::class, example = "2020-12")
-                                        val tomMåned: YearMonth?,
-                                        val beløp: Double,
-                                        val manueltBeregnet: Boolean,
-                                        val deltBosted: Boolean
+    data class UtvidetBarnetrygdPeriode(
+        val stønadstype: Stønadstype,
+        @Schema(implementation = String::class, example = "2020-05")
+        val fomMåned: YearMonth,
+        @Schema(implementation = String::class, example = "2020-12")
+        val tomMåned: YearMonth?,
+        val beløp: Double,
+        val manueltBeregnet: Boolean,
+        val deltBosted: Boolean,
     )
 
     enum class Stønadstype {
         UTVIDET,
-        SMÅBARNSTILLEGG
+        SMÅBARNSTILLEGG,
     }
 }
-
