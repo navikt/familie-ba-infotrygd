b0cc04c42cf99528a5ec8575829f5b721a68ba20
Stig Helge Strøm <stig.helge.strom@nav.no>
2025-02-14 13:43:44 +0100

    Lagt til systemtype på LogFilter

--- src/main/kotlin/no/nav/familie/ba/infotrygd/config/LogConfig.kt
+++ src/main/kotlin/no/nav/familie/ba/infotrygd/config/LogConfig.kt
@@ -1,6 +1,7 @@
 package no.nav.familie.ba.infotrygd.config
 
 import no.nav.familie.ba.infotrygd.rest.filter.AccessLogFilter
+import no.nav.familie.log.NavSystemtype
 import no.nav.familie.log.filter.LogFilter
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
@@ -8,12 +9,11 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.core.Ordered.LOWEST_PRECEDENCE
 
 @Configuration
-class  LogConfig {
-
+class LogConfig {
     @Bean
     fun logFilterFelles(): FilterRegistrationBean<LogFilter> {
         val filterRegistration: FilterRegistrationBean<LogFilter> = FilterRegistrationBean()
-        filterRegistration.filter = LogFilter()
+        filterRegistration.filter = LogFilter(NavSystemtype.NAV_INTEGRASJON)
         filterRegistration.order = 1
         return filterRegistration
     }
