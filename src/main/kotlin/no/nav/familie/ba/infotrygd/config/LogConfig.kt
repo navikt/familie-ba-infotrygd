13836004c77617af941587cba0e8246c9a55c43f
Stig Helge Strøm <stig.helge.strom@nav.no>
2026-03-27 14:27:14 +0100

    Sletter fil ikke i bruk

--- src/main/kotlin/no/nav/familie/ba/infotrygd/config/LogConfig.kt
+++ src/main/kotlin/no/nav/familie/ba/infotrygd/config/LogConfig.kt
@@ -1,12 +1,10 @@
 package no.nav.familie.ba.infotrygd.config
 
-import no.nav.familie.ba.infotrygd.rest.filter.AccessLogFilter
 import no.nav.familie.log.NavSystemtype
 import no.nav.familie.log.filter.LogFilter
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.Ordered.LOWEST_PRECEDENCE
 
 @Configuration
 class LogConfig {
